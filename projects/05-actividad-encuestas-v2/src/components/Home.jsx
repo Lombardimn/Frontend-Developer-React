67994d691144bfb4fcd9ad724a7e2a5d59edf3ba
Matias Lombardi <matiiaslombardi@gmail.com>
2023-10-11 10:30:40 -0300

    V.2.1.3 - Fixed link Home

--- projects/05-actividad-encuestas-v2/src/components/Home.jsx
+++ projects/05-actividad-encuestas-v2/src/components/Home.jsx
@@ -9,22 +9,21 @@ export function Home ({listQuestions}) {
             {
                 // eslint-disable-next-line react/prop-types
                 listQuestions.map( question => (
-                    <div 
-                        className="question-container"
-                        key={question.id}
-                    >
-                        <article className="question-item">
-                            <h2 className="question-title">
-                                {question.title}
-                            </h2>
-                            <p className="question-description">
-                                {question.description}
-                            </p>
-                            <Link className="question-a" to={`/survey/${question.id}`}>
-                                Ver +
-                            </Link>
-                        </article>
-                    </div>
+                    <Link key={question.id} className="question-a" to={`/survey/${question.id}`}>
+                        <div 
+                            className="question-container"
+                            // key={question.id}
+                        >
+                            <article className="question-item">
+                                <h2 className="question-title">
+                                    {question.title}
+                                </h2>
+                                <p className="question-description">
+                                    {question.description}
+                                </p>
+                            </article>
+                        </div>
+                    </Link>
                 ))
             }
         </section>
