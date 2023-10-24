457b9f072f238eb57ccf9c72705b8dc30f1c6247
Matias Lombardi <matiiaslombardi@gmail.com>
2023-10-24 13:08:51 -0300

    V.2.4.2 - Fixed project 04 deploy

--- projects/04-actividad_encuestas_app/src/app/page.js
+++ projects/04-actividad_encuestas_app/src/app/page.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Encuentas from '@/components/Encuentas';
-import '@/styles/page.css'
-import Footer from '@/components/Footer';
-import encuestas from '@/utils/encuestas.json'
+import Encuentas from '../components/Encuentas';
+import '../styles/page.css';
+import Footer from '../components/Footer';
+import encuestas from '../utils/encuestas.json';
 
 export default function App() {
     return (
--- projects/04-actividad_encuestas_app/src/components/Encuentas.jsx
+++ projects/04-actividad_encuestas_app/src/components/Encuentas.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from '@/styles/encuesta.module.css'
+import styles from '../styles/encuesta.module.css'
 
 export default function Encuentas({encuestas}) {
     return (
